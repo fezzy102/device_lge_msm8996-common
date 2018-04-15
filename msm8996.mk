760abe595d993741a9e7f585b16659b1695b07dc
Rashed Abdel-Tawab <rashed@linux.com>
2018-04-15 09:56:59 -0700

    msm8996: Move device properties to vendor
    Change-Id: If5c1ca4247c56ab2c87354f433d39d9fe624edb4

--- msm8996.mk
+++ msm8996.mk
@@ -23,6 +23,9 @@ $(call inherit-product, vendor/aquarios/config/utils.mk)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 # DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
+# Properties
+-include $(LOCAL_PATH)/vendor_prop.mk
+
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := 560dpi
